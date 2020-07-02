87c61dc70762f61de16c38de81e3bd6923b8986d
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2020-07-03 02:47:56 +0530

    fix: explictly set node: false if default export

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 module.exports = {
+	"env": {
+		// `airbnb-base` sets this to true. We reset it here,
+		// to make the config extend logic clearer.
+		"node": false
+	},
 	"extends": [
 		"airbnb-base", // `eslint-config-airbnb-base` rules
 	],
